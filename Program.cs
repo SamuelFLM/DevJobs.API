b18a5ba2fdadd5212c869cfe49382db60e92c48f
Samuel Felipe Lima <felipelimaaa23@gmail.com>
2023-09-23 21:31:25 -0300

    test

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddSingleton<DevJobsContext>();
 
-
+Console.WriteLine("");
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
